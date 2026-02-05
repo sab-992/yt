bcb99c0a458232a65269543784300241ca85b084
sab-992 <71243885+sab-992@users.noreply.github.com>
2026-02-05 14:59:45 -0500

    added a try catch for errors and ease of use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.tasks.fetch_video import FetchVideo
 from src.threads.threads import Threads
+from src.utils.logger import Logger, Level
 from src.video.video import Video
 
 
@@ -8,17 +9,21 @@ RESOLUTION = "1920x1080" # 1920x1080 or 1080p works !
 FPS: int = 60 # Enter a number or None if not available for the video.
 
 def main():
-    video = Video(url=VIDEO_URL, resolution="1920x1080", fps=FPS)
+    try:
+        video = Video(url=VIDEO_URL, resolution="1920x1080", fps=FPS)
 
-    threads = Threads()
-    threads.start_all()
+        threads = Threads()
+        threads.start_all()
 
-    for _ in range(len(threads)):
-        threads.add_task(FetchVideo(video=video, thread_count=len(threads)))
+        for _ in range(len(threads)):
+            threads.add_task(FetchVideo(video=video, thread_count=len(threads)))
 
-    results: dict = threads.stop_all()
+        results: dict = threads.stop_all()
 
-    video.save(results)
+        video.save(results)
+    except Exception as e:
+        Logger.log("Unexpected behavior, the video might've not been downloaded successfully. Error will follow", level=Level.WARNING, to_std_out=True)
+        Logger.log(e, level=Level.WARNING, to_std_out=True)
 
 if __name__ == "__main__":
     main()
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -20,20 +20,21 @@ class Logger:
 
     @staticmethod
     def log(message: str, level: Level=Level.INFO, content: str="", file_name:str=random.getrandbits(MAX_LOG_HASH_FILENAME_SIZE), folder_path=LOG_FOLDER_PATH, to_std_out:bool=PRINT_TO_STDOUT):
+        formatted_message: str = f"{level.value} - Timestamp: {datetime.now().strftime("%Y-%m-%dT%H:%M:%S")} -> {message}."
+        with mutex:
+            if to_std_out:
+                print(formatted_message)
+
         if not ENABLE_LOGGING:
             return message
 
         detailed_file_path = f"{folder_path}/details/{file_name}.txt"
-        formatted_message: str = f"{level.value} - Timestamp: {datetime.now().strftime("%Y-%m-%dT%H:%M:%S")} -> {message}."
 
         with mutex:
             if len(content) > 0:
                 formatted_message += f" Written at: { str(PureWindowsPath(detailed_file_path) if platform.system() == "Windows" else PurePosixPath(detailed_file_path)) }\n"
                 with open(detailed_file_path, "w", encoding="utf-8") as f:
                     f.write(content)
-
-            if to_std_out:
-                print(formatted_message)
 
             with open(f"{folder_path}/logs.txt", "a", encoding="utf-8") as f:
                 f.write(formatted_message + "\n")
